6607c494d7916637f4aa6d423ca6952ac2a648f1
Ankitsehrawat01 <sehrawata328@gmail.com>
2023-08-28 09:42:28 +0530

    [Ankit Kumar]: Added Employee Present/Absent

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EmployeeWage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int present = 1;
+
+            Random random = new Random();
+            int empCheck = random.Next(0,2);
+            if (empCheck == present)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
